bcdf791ad060092b34d00bbd4af76d8d62a81925
Sam <sammy.unterman@gmail.com>
2016-06-07 17:26:19 -0400

    finished caluclator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -5,3 +5,18 @@ end
 def subtract(int1, int2)
   return int1 - int2
 end
+
+def sum(my_arr)
+
+  place_holder = 0
+  if my_arr.length == 0
+    return 0
+  elsif my_arr.length == 1
+    return my_arr[0]
+  elsif my_arr.length > 1
+    my_arr.each { |x| place_holder += x }
+    return place_holder
+  end
+
+
+end
